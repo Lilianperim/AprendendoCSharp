6b17554b84563d44b5161643feedb982d8a03e5f
Lilian Perim <liliancrisperim@gmail.com>
2021-04-27 21:50:59 -0300

    Curso 2

---
+++ BiteBank/01-BiteBank/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_BiteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaLilian = new ContaCorrente();
+
+            contaDaLilian.titular = "Lilian";
+            contaDaLilian.agencia = 863;
+            contaDaLilian.numero = 563142;
+            contaDaLilian.saldo = 100;
+
+
+            Console.WriteLine("Nome: " + contaDaLilian.titular);
+            Console.WriteLine("Agência: " + contaDaLilian.agencia);
+            Console.WriteLine("Nº Conta: " + contaDaLilian.numero);
+            Console.WriteLine("Saldo: R$" + contaDaLilian.saldo);
+
+            //contaDaLilian.saldo += 200; *atualizando saldo* 
+            // Console.WriteLine("Saldo: R$" + contaDaLilian.saldo);
+
+      
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ BiteBank/03-ByteBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaLilian = new ContaCorrente(); //referência/instância (contaDaLilian) para a criação do objeto Contacorrente
+            contaDaLilian.titular = "Lilian";
+            contaDaLilian.agencia = 863;
+            contaDaLilian.numero = 123456;
+
+            ContaCorrente contaDoCleber = new ContaCorrente(); 
+            contaDoCleber.titular = "Cleber";
+            contaDoCleber.agencia = 863;
+            contaDoCleber.numero = 123456;
+
+            Console.WriteLine("Igualdade de tipo de referência:" + (contaDaLilian == contaDoCleber));
+
+            int idade = 27;
+            int idadeMaisUmaVez = 27;
+
+            Console.WriteLine("Igualdade de tipo valor:" + (idade == idadeMaisUmaVez));
+
+            contaDaLilian = contaDoCleber;
+            Console.WriteLine(contaDaLilian == contaDoCleber);
+
+            contaDaLilian.saldo = 300;
+            Console.WriteLine(contaDaLilian.saldo); 
+            Console.WriteLine(contaDoCleber.saldo);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
